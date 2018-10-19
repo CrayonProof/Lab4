ca3959bb979b6afa8cf6024ebef2924780e270ee
Jacob Crandall <crandall@cs.byu.edu>
2018-10-19 04:29:36 +0000

    working

--- Pathfinder.cpp
+++ Pathfinder.cpp
@@ -8,11 +8,23 @@ using namespace std;
 
 static const int MAZE_SIZE = 5;
 int currentMaze[MAZE_SIZE][MAZE_SIZE][MAZE_SIZE];
+int tempMaze[MAZE_SIZE][MAZE_SIZE][MAZE_SIZE];
 vector<string> path;
 
 Pathfinder::Pathfinder()
 {
     srand (time(NULL));
+    
+    for (int z = 0; z < MAZE_SIZE; z++)
+    {
+            for (int y = 0; y < MAZE_SIZE; y++)
+        {
+                for (int x = 0; x < MAZE_SIZE; x++)
+            {
+                currentMaze[x][y][z] = 1;
+            }
+        }
+    }
 }
 
 
@@ -61,11 +73,12 @@ void Pathfinder::createRandomMaze()
 
 bool Pathfinder::importMaze(string file_name)
 {
+    vector<char> charVec;
     
     ifstream inputFile;
     inputFile.open(file_name);
     
-    char ch;
+    char c;
     char preCh = 'o';
     fstream fin(file_name, fstream::in);
     
@@ -73,8 +86,37 @@ bool Pathfinder::importMaze(string file_name)
     int y = 0;
     int z = 0;
     
+    int cCount = 0;
     //cout << inputFile.rdbuf() << endl;
-    while (fin >> noskipws >> ch)
+    while (fin >> noskipws >> c)
+    {
+        
+        if (c == '1' || c == '0' || c == ' ' || c == '\n')
+        {
+            charVec.push_back(c);
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    for (char c : charVec)
+    {
+        if(c == '1' || c == '0')
+        {
+            cCount++;
+        }
+    }
+    
+    if(cCount != MAZE_SIZE*MAZE_SIZE*MAZE_SIZE)
+    {
+        return false;
+    }
+    
+    inputFile.close();
+    
+    for (char ch : charVec)
     {
         //cout << ch << endl;
         if (ch == '0')
@@ -116,8 +158,13 @@ bool Pathfinder::importMaze(string file_name)
             inputFile.close();
             return false;
         }
+        
+        if((((x == 0) && (y == 0) && (z == 0)) || ((x == MAZE_SIZE - 1) && (y == MAZE_SIZE - 1) && (z == MAZE_SIZE - 1))) && ch != '1')
+        {
+            return false;
+        }
     }
-    inputFile.close();
+    
     return true;
 }
 
@@ -126,21 +173,48 @@ bool outOfBounds(int x, int a, int z)
     return (x < a || x > z);
 }
 
-bool findPath (int x, int y, int z)
+bool Pathfinder::findPath (int x, int y, int z)
 {
-    path.push_back("(" + to_string(x) + "," + to_string(y) + "," + to_string(z) + ")");
     
-    bool xB = outOfBounds(x, 0, MAZE_SIZE);
-    bool yB = outOfBounds(y, 0, MAZE_SIZE);
-    bool zB = outOfBounds(y, 0, MAZE_SIZE);
-    
-    if(xB || yB || zB || currentMaze[x][y][z] !=1)
+    cout << "maze1" << endl;
+    /*
+    for (int zz = 0; zz < MAZE_SIZE; zz++)
     {
+            for (int yy = 0; yy < MAZE_SIZE; yy++)
+        {
+                for (int xx = 0; xx < MAZE_SIZE; xx++)
+            {
+               cout << currentMaze[xx][yy][zz] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+    cout << endl;
+    */
+    
+    path.push_back("(" + to_string(x) + ", " + to_string(y) + ", " + to_string(z) + ")");
+    
+    bool xB = outOfBounds(x, 0, MAZE_SIZE - 1);
+    bool yB = outOfBounds(y, 0, MAZE_SIZE - 1);
+    bool zB = outOfBounds(z, 0, MAZE_SIZE - 1);
+    
+    if(!(x >= 0 && x < MAZE_SIZE) || !(y >= 0 && y < MAZE_SIZE) || !(z >= 0 && z < MAZE_SIZE))
+    {
+        //cout << "ONEONEONE: " << currentMaze[x][y][z] << endl;
         path.pop_back();
         return false;
     }
-    if (x >= MAZE_SIZE && y >= MAZE_SIZE && z >= MAZE_SIZE)
+    if(currentMaze[x][y][z] != 1)
     {
+        //cout << "xyz " << x << y << z << endl;
+        //cout << "ZUBZUBZUB: " << currentMaze[x][y][z] << endl;
+        path.pop_back();
+        return false;
+    }
+    if (x >= MAZE_SIZE - 1 && y >= MAZE_SIZE - 1 && z >= MAZE_SIZE - 1)
+    {
+        //cout << "TWOTWOTWO" << endl;
         return true;
     }
     
@@ -148,11 +222,15 @@ bool findPath (int x, int y, int z)
     
     if(findPath(x + 1, y, z) || findPath(x - 1, y, z) || findPath(x, y + 1, z) || findPath(x, y - 1, z) || findPath(x, y, z + 1) || findPath(x, y, z - 1))
     {
+        //cout << "THREETHREETHREE" << endl;
+        //currentMaze[x][y][z] = 1;
         return true;
     }
     else
     {
-        path.pop_back();
+        //cout << "FOURFOURFOUR" << endl;
+        //path.pop_back();
+        //currentMaze[x][y][z] = 1;
         return false;
     }
 }
@@ -161,10 +239,47 @@ vector<string> Pathfinder::solveMaze()
 {
     for (int i = 0; i < path.size(); i++)
     {
-        path.erase(path.begin());
+        path.pop_back();
     }
     
+    
+    for (int z = 0; z < MAZE_SIZE; z++)
+    {
+            for (int y = 0; y < MAZE_SIZE; y++)
+        {
+                for (int x = 0; x < MAZE_SIZE; x++)
+            {
+               tempMaze[x][y][z] = currentMaze[x][y][z];
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+    cout << endl;
+    
+    
+    
     findPath(0, 0, 0);
+    
+    for (int z = 0; z < MAZE_SIZE; z++)
+    {
+            for (int y = 0; y < MAZE_SIZE; y++)
+        {
+                for (int x = 0; x < MAZE_SIZE; x++)
+            {
+               currentMaze[x][y][z] = tempMaze[x][y][z];
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+    cout << endl;
+    
+    cout << "path: " << endl;
+    for(string s : path)
+    {
+        cout << s << endl;
+    }
     return path;
     
 }
--- Pathfinder.h
+++ Pathfinder.h
@@ -12,8 +12,11 @@ class Pathfinder :
         string toString() const;
         void createRandomMaze();
         bool importMaze(string file_name);
+        bool findPath(int x, int y, int z);
         vector<string> solveMaze();
     private:
         static const int MAZE_SIZE = 5;
         int currentMaze[MAZE_SIZE][MAZE_SIZE][MAZE_SIZE];
+        int temptMaze[MAZE_SIZE][MAZE_SIZE][MAZE_SIZE];
+        vector<string> path;
 };
